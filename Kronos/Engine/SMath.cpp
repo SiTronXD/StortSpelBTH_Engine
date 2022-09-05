8b1810c81baf3a20254e1d3249067c23003829d4
lr222gw <lowe.raivio@gmail.com>
2022-09-05 13:14:47 +0200

    Delete Kronos directory

--- Kronos/Engine/SMath.cpp
+++
@@ -1,3 +0,0 @@
-#include "SMath.h"
-
-const float SMath::PI = 3.141592f;
--- Kronos/Resources/Shaders/compile.bat
+++
@@ -1,3 +0,0 @@
-C:/VulkanSDK/1.3.216.0/Bin/glslc.exe VertexShaders/Default.vert -o vert.spv
-C:/VulkanSDK/1.3.216.0/Bin/glslc.exe FragmentShaders/Default.frag -o frag.spv
-pause
