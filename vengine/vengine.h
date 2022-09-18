655f9b1597f97eea60a1df9a17f746ecc41baf71
William Kamnert <wille.kamnert@gmail.com>
2022-09-18 22:02:19 +0200

    RFA: Linking and includes
    Fixed the linking and including of directory problem that was present when using the vengine.lib in the demos. This process is now more dynamic and easy to set up, the only thing needed is to set a reference to the vengine project and including vengine.h.

--- vengine/vengine.h
+++ vengine/vengine.h
@@ -1,9 +1,9 @@
 #pragma once 
 
-#include "src/Engine.hpp"
-#include "src/TestScene.hpp"
-#include "imgui/imgui.h"
-#include "src/Input.hpp"
-#include "src/MeshComponent.hpp"
-#include "src/Log.hpp"
-#include "src/Time.hpp"
+#include "vengine/Engine.hpp"
+#include "vengine/TestScene.hpp"
+#include "vengine/imgui/imgui.h"
+#include "vengine/Input.hpp"
+#include "vengine/MeshComponent.hpp"
+#include "vengine/Log.hpp"
+#include "vengine/Time.hpp"
