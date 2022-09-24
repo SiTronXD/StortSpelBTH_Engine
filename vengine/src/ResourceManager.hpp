746c7c80e7c99659b749788935c704258e03806d
lr222gw <lowe.raivio@gmail.com>
2022-09-24 14:52:46 +0200

    ADD: ResourceMan decls: Methods, new members
    -Set Engine as a friendclass: Needs to run cleanup method.
    -Set VulkanRenderer as friendclass: Needs to run resourceManagers init
    -Add support for storing texture
    -Add Getters for Mesh and Texture instances

--- vengine/src/ResourceManager.hpp
+++ vengine/src/ResourceManager.hpp
@@ -4,17 +4,31 @@
 #include <unordered_map>
 #include <string>
 
-class Model;
+//class Model;
+class NewModel;
+class Engine;
+struct ImageData;   //Defined in MeshLoader
 
 class ResourceManager{
 private:
+    /// Engine should take care of cleanups
+    friend class Engine; 
+    friend class VulkanRenderer; 
     ResourceManager(); // Private Constructor, do not allow ResourceManager to be created
-    static std::unordered_map<std::string, uint32_t> mesh_paths;
-    static std::unordered_map<uint32_t, Model> meshes;
+    static std::unordered_map<std::string, uint32_t> meshPaths;
+    static std::unordered_map<std::string, uint32_t> texturePaths;
+    
+    static std::unordered_map<uint32_t, NewModel>  meshes;
+    static std::unordered_map<uint32_t, ImageData> textures;
 
+    static void cleanup();
 public:
 
-    static uint32_t addMesh(std::string&& mesh_path);
+    static uint32_t addMesh(std::string&& meshPath);
+    static uint32_t addTexture(std::string&& texturePath);
+
+    static NewModel&     getMesh(uint32_t id);      //TODO: inline when not static
+    static ImageData&    getTexture(uint32_t id);   //TODO: inline when not static
 };
 
 /*
