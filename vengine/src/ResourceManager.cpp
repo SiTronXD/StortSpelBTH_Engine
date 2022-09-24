08c594ba552cb47bbc12ee8e6f12ad50c690213d
lr222gw <lowe.raivio@gmail.com>
2022-09-24 16:50:57 +0200

    ADD: define ResourceManager addTexture

--- vengine/src/ResourceManager.cpp
+++ vengine/src/ResourceManager.cpp
@@ -5,6 +5,36 @@
 
 std::unordered_map<std::string, uint32_t>   ResourceManager::meshPaths;
 std::unordered_map<std::string, uint32_t>   ResourceManager::texturePaths;
+std::unordered_map<uint32_t, NewModel>      ResourceManager::meshes;
+std::unordered_map<uint32_t, ImageData>     ResourceManager::textures;
+
+uint32_t ResourceManager::addTexture(std::string&& texturePath)
+{ 
+    using namespace vengine_helper::config;    
+    
+    uint32_t prevSize = ResourceManager::texturePaths.size();
+    ResourceManager::texturePaths.insert({texturePath,prevSize}); //NOTE: prevSize as key only works if we never remove resources the map...
+
+    // If exists, return key of existing mesh
+    if(ResourceManager::texturePaths.size() == prevSize)
+    {
+        Log::warning("Texture [TODO: insert name of mesh file] was already added!"); //TODO: should be able to log what mesh
+        
+        
+        return ResourceManager::textures.find(
+                ResourceManager::texturePaths.find(texturePath)->second
+        )->first;
+    }
+
+    auto textureResource = TextureLoader::createTexture(DEF<std::string>(P_TEXTURES) + texturePath); //NOTE: meshes.size as key only works if we never remove resources the map...
+
+    // Create mesh, insert into map of meshes
+    textures.insert({
+        textures.size(),
+        textureResource});       
+
+    return textures.size() -1;
+}
 
 
 
