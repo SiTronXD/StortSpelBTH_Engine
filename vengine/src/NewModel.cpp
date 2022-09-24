5ceecf0cba0fa9d7ffe1398cf8ead2f9b69cecbd
lr222gw <lowe.raivio@gmail.com>
2022-09-24 17:06:01 +0200

    ADD: NewModel moveconstructor definition

--- vengine/src/NewModel.cpp
+++ vengine/src/NewModel.cpp
@@ -6,3 +6,15 @@ NewModel::NewModel(MeshData&& meshData, VulkanImportStructs& importStructs)
 
 }
 
+
+
+NewModel::NewModel(NewModel&& ref)
+:   submeshData(std::move(ref.submeshData)),
+    device(ref.device),
+    vma(ref.vma),
+    vertexBuffer(std::move(ref.vertexBuffer)),
+    indexBuffer(std::move(ref.indexBuffer)),
+    vertexBufferMemory(std::move(ref.vertexBufferMemory)),
+    indexBufferMemory(std::move(ref.indexBufferMemory))
+{
+}
