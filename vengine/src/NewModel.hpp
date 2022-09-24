ce48f9a368e0ca20f8f94b8c17e4891a0e531541
lr222gw <lowe.raivio@gmail.com>
2022-09-24 15:13:44 +0200

    ADD: new class, will replace old Mesh class later
    - Created new class but intend to keep the old Model and Mesh class
    to compare with.

---
+++ vengine/src/NewModel.hpp
@@ -0,0 +1,31 @@
+#pragma once 
+
+#include <vulkan/vulkan.hpp>
+
+#include "ResourceManagerStructs.hpp"
+#include "CommonMeshStructs.hpp"
+#include "glm/matrix.hpp"
+#include "vk_mem_alloc.h"
+
+
+class NewModel{
+private: 
+    std::vector<SubMeshData>    submeshData;
+    Device&         device; 
+    VmaAllocator&   vma;     
+
+    vk::Buffer  vertexBuffer{};
+    vk::Buffer  indexBuffer{};
+    VmaAllocation vertexBufferMemory{};
+    VmaAllocation indexBufferMemory{};
+public:     
+    NewModel(MeshData&& meshData, VulkanImportStructs& importStructs);
+    NewModel(NewModel&& ref);
+    void createVertexBuffer(MeshData& meshData, VulkanImportStructs& importStructs);
+    void createIndexBuffer( MeshData& meshData, VulkanImportStructs& importStructs);
+    vk::Buffer& getVertexBuffer();
+    vk::Buffer& getIndexBuffer( );
+    std::vector<SubMeshData> getSubMeshData();
+
+    void cleanup();
+};
