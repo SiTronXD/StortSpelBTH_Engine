9028222db91b2841636fa451543487a7d985661b
lr222gw <lowe.raivio@gmail.com>
2022-09-25 13:00:52 +0200

    RFA:** Reorganizing folder structure
    Note: this commit is meant to help upcoming merge. It will not compile since includes are all wrong. Neither CMakeLists or .sln is updated, but will be after merge.

--- vengine/vengine/graphics/CommonMeshStructs.hpp
+++ vengine/vengine/graphics/CommonMeshStructs.hpp
@@ -5,7 +5,7 @@
 #include <glm/vec2.hpp>
 #include <glm/mat4x4.hpp>
 
-const uint32_t MAX_SUBMESH_MATERIAL_NAME_LENGTH = 64;
+const uint32_t MAX_SUBMESH_MATERIAL_NAME_LENGTH = 64; //TODO REMOVE
 
 struct ModelMatrix { 
     glm::mat4 model; 
