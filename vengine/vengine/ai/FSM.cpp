81d4178cbfba7dedcd30e7dc6b4a9283b91bb90b
Oscar Nordström <oscar.nordstrom2000@gmail.com>
2022-10-27 12:22:49 +0200

    Event system fix

--- vengine/vengine/ai/FSM.cpp
+++ vengine/vengine/ai/FSM.cpp
@@ -1,3 +1,17 @@
 #include "FSM.hpp"
+#include "../components/FSMAgentComponent.hpp"
 
 SceneHandler* FSM::sceneHandler = nullptr;
+
+void FSM::execute(uint32_t entityID) 
+{ 
+	FSM_Node* node = sceneHandler->getScene()->getComponent<FSMAgentComponent>(entityID).currentNode;
+	if(node)
+	{
+		node->execute(entityID);
+	}
+	else
+	{
+		Log::error("Failed: FSM_Node is a nullptr!");
+	} 
+}
