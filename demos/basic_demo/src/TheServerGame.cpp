4b9327e8555faabead0fa955a1f3cf9df7132285
SimonStrands <etta11@hotmail.se>
2022-11-04 14:57:56 +0100

    test scene

--- demos/basic_demo/src/TheServerGame.cpp
+++ demos/basic_demo/src/TheServerGame.cpp
@@ -11,9 +11,12 @@ void TheServerGame::init()
     this->getComponent<Transform>(ground).position = glm::vec3(0, -1, 0);
     this->setComponent<Collider>(ground, Collider::createBox(glm::vec3(100, 0.2, 100)));
 
-    int e = this->createEnemy(1,"", glm::vec3(0,0,5));
-    this->setComponent<Collider>(e, Collider::createBox(glm::vec3(100, 0.2, 100)));
+    int e = this->createEnemy(1,"", glm::vec3(0,4,0));
+    this->setComponent<Collider>(e, Collider::createBox(glm::vec3(1, 1, 1)));
     this->setComponent<Rigidbody>(e);
+    this->getComponent<Rigidbody>(e).rotFactor = glm::vec3(0, 0, 0);
+    e = this->createEnemy(1, "", glm::vec3(4, 5, 0));
+    this->setComponent<Collider>(e, Collider::createBox(glm::vec3(1, 1, 1)));
 }
 
 void TheServerGame::update(float dt)
