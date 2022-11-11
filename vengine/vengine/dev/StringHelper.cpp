b4ed4ffeb4e9fa2e5795126e51492066e16a4268
William Kamnert <wille.kamnert@gmail.com>
2022-11-11 13:05:14 +0100

    FIX: String helper

--- vengine/vengine/dev/StringHelper.cpp
+++ vengine/vengine/dev/StringHelper.cpp
@@ -19,23 +19,3 @@ void StringHelper::splitString(
 	// Add remaining string
 	output.emplace_back(str);
 }
-
-void StringHelper::splitString(
-	const std::string& str, 
-	const char& delim, 
-	std::vector<std::string>& output)
-{
-	size_t pos = 0;
-	std::string strCopy = str;
-	while ((pos = strCopy.find(delim)) != std::string::npos)
-	{
-		// Extract and add token
-		output.emplace_back(strCopy.substr(0, pos));
-
-		// Remove front
-		strCopy.erase(0, pos + 1);
-	}
-
-	// Add remaining string
-	output.emplace_back(strCopy);
-}
--- vengine/vengine/dev/StringHelper.hpp
+++ vengine/vengine/dev/StringHelper.hpp
@@ -8,6 +8,4 @@ class StringHelper
 public:
 	static void splitString(std::string& str, const char& delim,
 		std::vector<std::string>& output);
-	static void splitString(const std::string& str, const char& delim,
-		std::vector<std::string>& output);
 };
