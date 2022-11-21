0551e1bbe1baa74cbdccb87662cba21770960b78
lowe <lowe.raivio@gmail.com>
2022-11-21 09:10:41 +0100

    ADD: Imgui support for different FSMs

--- vengine/vengine/ai/AIHandler.cpp
+++ vengine/vengine/ai/AIHandler.cpp
@@ -17,58 +17,64 @@ void AIHandler::drawImgui(){
     float width = ImGui::GetWindowWidth();
     if(ImGui::BeginTabBar("BlackboardTabs", ImGuiTabBarFlags_None)){
         
-        if (ImGui::BeginTabItem("Entities"))
-        {   
+        for(auto fsm : this->FSMs)
+        {
+            const std::string&  fsmName = fsm.first;
+            FSM*          fsmPtr = fsm.second;
+            if (ImGui::BeginTabItem(fsmName.c_str()))
+            {   
 
-            static std::string entity_label;
-            static uint32_t selected_entity = 0;                
-            static std::function<void(FSM*, uint32_t)> selected_entity_func;                
-            static FSM* selected_fsm = nullptr;
-            
-            ImGui::SetNextItemOpen(blackboard_Entities_open, ImGuiCond_FirstUseEver);
-            ImGui::BeginChild("entity_picker", ImVec2(100, 0), false, ImGuiWindowFlags_AlwaysAutoResize);
+                static std::string entity_label;
+                static uint32_t selected_entity = 0;                
+                static std::function<void(FSM*, uint32_t)> selected_entity_func;                
+                static FSM* selected_fsm = nullptr;
+                
+                ImGui::SetNextItemOpen(blackboard_Entities_open, ImGuiCond_FirstUseEver);
+                ImGui::BeginChild("entity_picker", ImVec2(100, 0), false, ImGuiWindowFlags_AlwaysAutoResize);
 
-            // Use first as default! 
-            if(this->switchedScene)
-            {
-                selected_entity      = this->FSMsEntities[this->FSMimguiLambdas.begin()->first][0];                        
-                selected_entity_func = this->FSMimguiLambdas.begin()->second;
-                selected_fsm         = this->FSMimguiLambdas.begin()->first;
-            }
-                        
-            for(auto fsmImguiLambda : this->FSMimguiLambdas)
-            {
-                for(auto entityId : this->FSMsEntities[fsmImguiLambda.first])
+                // Use first as default! 
+                if(this->switchedScene)
+                {
+                    selected_entity      = this->FSMsEntities[this->FSMimguiLambdas.begin()->first][0];                        
+                    selected_entity_func = this->FSMimguiLambdas.begin()->second;
+                    selected_fsm         = this->FSMimguiLambdas.begin()->first;
+                }
+                            
+                auto fsmImguiLambda = this->FSMimguiLambdas[fsmPtr];
+                
+                for(auto entityId : this->FSMsEntities[fsmPtr])
                 {
                     
                     entity_label = "Entity["+std::to_string(entityId)+"]";
                     if(ImGui::Selectable(entity_label.c_str(), selected_entity == entityId) )
                     { 
                         selected_entity = entityId;                        
-                        selected_entity_func = fsmImguiLambda.second;
-                        selected_fsm = fsmImguiLambda.first;
+                        selected_entity_func = fsmImguiLambda;
+                        selected_fsm = fsmPtr;
                     }  
                 }
-            }    
-            ImGui::EndChild();
-            ImGui::SameLine();        
-            ImGui::BeginGroup();
-            ImGui::BeginChild("Entity_view",ImVec2(0, 0), false, ImGuiWindowFlags_AlwaysAutoResize); 
-            ImGui::Text("Entity[%d]", selected_entity);
-            ImGui::Separator();
-            if (ImGui::BeginTabBar("Tabs", ImGuiTabBarFlags_None))
-            {
-                if (ImGui::BeginTabItem("Data"))
-                {                
-                    selected_entity_func(selected_fsm,selected_entity);                    
-                    ImGui::EndTabItem();
-                }
+                    
+                ImGui::EndChild();
+                ImGui::SameLine();        
+                ImGui::BeginGroup();
+                ImGui::BeginChild("Entity_view",ImVec2(0, 0), false, ImGuiWindowFlags_AlwaysAutoResize); 
+                ImGui::Text("Entity[%d]", selected_entity);
+                ImGui::Separator();
+                if (ImGui::BeginTabBar("Tabs", ImGuiTabBarFlags_None))
+                {
+                    if (ImGui::BeginTabItem("Data"))
+                    {                
+                        selected_entity_func(selected_fsm,selected_entity);                    
+                        ImGui::EndTabItem();
+                    }
 
-                ImGui::EndTabBar();
+                    ImGui::EndTabBar();
+                }
+                ImGui::EndChild();
+                ImGui::EndGroup();
+                ImGui::EndTabItem();
             }
-            ImGui::EndChild();
-            ImGui::EndGroup();
-            ImGui::EndTabItem();
+
         }
 
         ImGui::EndTabBar();
