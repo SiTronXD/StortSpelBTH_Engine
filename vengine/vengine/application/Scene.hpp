2c9827b2a2036aa569158ca2fa67a94946111c95
Olowerr <oliver.klingspor@hotmail.com>
2022-11-28 10:53:02 +0100

    Fix: Comment

--- vengine/vengine/application/Scene.hpp
+++ vengine/vengine/application/Scene.hpp
@@ -117,7 +117,7 @@ public:
 	void blendToAnimation(Entity entity, const std::string& animationName, const std::string& slotName = "", float transitionTime = 0.18f, float nextAniTimeScale = 1.f);
 	void syncedBlendToAnimation(Entity entity, const std::string& referenceSlot, const std::string& slotToSync = "", float transitionTime = 0.18);
 	AnimationSlot& getAnimationSlot(Entity entity, const std::string& slotName);
-	// Assumes the whole skeleton is playing an animation, takes slot 0
+	// Assumes the whole skeleton is playing an animation, uses slot 0
 	AnimationStatus getAnimationStatus(Entity entity, const std::string& slotName = ""); 
 
 	// When created
