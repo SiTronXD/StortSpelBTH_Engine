497d5a8e0c7453cfcb724d79156747aafc347eb4
lr222gw <lowe.raivio@gmail.com>
2022-12-04 14:01:16 +0100

    FIX: Remove old new overloads in defs

--- vengine/vengine/dev/defs.cpp
+++ vengine/vengine/dev/defs.cpp
@@ -3,20 +3,22 @@
 #include <string>
 #include "tracy/Tracy.hpp"
 
-/// Operator overloading of new, used to enable tracy memory profiling!
-void* operator new( std::size_t count )
-{
-    std::unique_ptr<void*> f;
-    auto *ptr = malloc( count );    //:NOLINT: overidden new operator, must use malloc
-    TracySecureAlloc( ptr, count );
-    return ptr;
-}
+// /// Operator overloading of new(__FILE__, __LINE__), used to enable tracy memory profiling!
+// void* operator new(__FILE__, __LINE__)( std::size_t count )
+// {
+//     std::unique_ptr<void*> f;
+//     auto *ptr = malloc( count );    //:NOLINT: overidden new(__FILE__, __LINE__) operator, must use malloc
+//     TracySecureAlloc( ptr, count );
+//     return ptr;
+// }
 
-void operator delete( void* ptr ) noexcept
-{
-    TracySecureFree( ptr );
-    free( ptr );                    //:NOLINT: overidden delete operator, must use free
-}
+// void operator delete( void* ptr ) noexcept
+// {
+//     TracySecureFree( ptr );
+//     free( ptr );                    //:NOLINT: overidden delete operator, must use free
+// }
+
+
 
 void* CustomAlloc( size_t count )
 {
--- vengine/vengine/dev/defs.hpp
+++ vengine/vengine/dev/defs.hpp
@@ -1,17 +1,18 @@
-#pragma once
+#pragma once 
+ #include "op_overload.hpp"
 #include <memory>
-/// This will later be replaced by a class that parses a .cfg/.ini-file with these values
-/// in order to easily change constants
+// /// This will later be replaced by a class that parses a .cfg/.ini-file with these values
+// /// in order to easily change constants
 
 
 
-// const std::string PATH_ASSETS    =   "assets/";
-// const std::string PATH_MODELS    =   PATH_ASSETS+"models/";
-// const std::string PATH_TEXTURES  =   PATH_ASSETS+"textures/";
-// const std::string PATH_SHADERS   =   PATH_ASSETS+"shaders/";
+// // const std::string PATH_ASSETS    =   "assets/";
+// // const std::string PATH_MODELS    =   PATH_ASSETS+"models/";
+// // const std::string PATH_TEXTURES  =   PATH_ASSETS+"textures/";
+// // const std::string PATH_SHADERS   =   PATH_ASSETS+"shaders/";
 
 
-/// Custom Allocator Functions, used to enable Tracy Memory Profiling...
+// /// Custom Allocator Functions, used to enable Tracy Memory Profiling...
 
 void* CustomAlloc( size_t count );
 void CustomFree( void* ptr );
