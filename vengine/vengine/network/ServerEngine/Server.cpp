0d5379e86127c3d40aca3e8dda287ef9cd09d867
SimonStrands <etta11@hotmail.se>
2022-12-13 15:19:56 +0100

    remove player if one leaves

--- vengine/vengine/network/ServerEngine/Server.cpp
+++ vengine/vengine/network/ServerEngine/Server.cpp
@@ -259,6 +259,7 @@ void Server::handleDisconnects(int clientID)
 	delete clients[clientID];
 
 	this->sceneHandler.getScene()->onDisconnect(clientID);
+    this->sceneHandler.getScene()->removePlayer(clientID);
 	this->clients.erase(clients.begin() + clientID);
 	this->clientToServerPacketTcp.erase(clientToServerPacketTcp.begin() + clientID);
 	this->serverToClientPacketTcp.erase(serverToClientPacketTcp.begin() + clientID);
