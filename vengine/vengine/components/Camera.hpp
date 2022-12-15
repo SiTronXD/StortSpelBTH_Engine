718b0cf2bc86488f3d1da7bb08e8359bc8ba5fc8
Simon <47039664+SiTronXD@users.noreply.github.com>
2022-12-15 09:53:42 +0100

    ADD: camera flip aspect scale

--- vengine/vengine/components/Camera.hpp
+++ vengine/vengine/components/Camera.hpp
@@ -4,37 +4,77 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "Transform.hpp"
 
+#include "../dev/Log.hpp"
+
 struct Camera
 {
 private:
 	Transform oldTransform;
+
+	bool flipAspectScale = false;
+
+	void getPerspectiveMatrix(
+		float fov,
+		float aspectRatio,
+		float nearPlane,
+		float farPlane,
+		glm::mat4& output)
+	{
+		float tanHalfAngle = std::tan(fov * 0.5f);
+
+		const float DEFAULT_ASP = 16.0f / 9.0f;
+
+		// Modified projection matrix
+		if (this->flipAspectScale && aspectRatio < DEFAULT_ASP)
+		{
+			output = glm::mat4(
+				1.0f / (DEFAULT_ASP * tanHalfAngle), 0.0f, 0.0f, 0.0f,
+				0.0f, (aspectRatio / DEFAULT_ASP) / (tanHalfAngle), 0.0f, 0.0f,
+				0.0f, 0.0f, -farPlane / (farPlane - 2.0f * nearPlane), -1.0f,
+				0.0f, 0.0f, -2.0f * (farPlane * nearPlane) / (farPlane - nearPlane), 0.0f
+			);
+		}
+		// Original projection matrix
+		else
+		{
+			// Equivalent to 
+			// glm::perspective(fov, aspectRatio, nearPlane, farPlane)
+			output = glm::mat4(
+				1.0f / (aspectRatio * tanHalfAngle), 0.0f, 0.0f, 0.0f,
+				0.0f, 1.0f / (tanHalfAngle), 0.0f, 0.0f,
+				0.0f, 0.0f, -farPlane / (farPlane - 2.0f * nearPlane), -1.0f,
+				0.0f, 0.0f, -2.0f * (farPlane * nearPlane) / (farPlane - nearPlane), 0.0f
+			);
+		}
+	}
+
 public:
 	float fov;
 	float nearPlane;
 	float farPlane;
-    float aspectRatio;
+	float aspectRatio;
 
 	glm::mat4 view;
 	glm::mat4 projection;
 	glm::mat4 invProjection;
 	glm::mat4 viewAndProj;
 
-	Camera(float fov = 90.0f) 
-		: fov(fov), aspectRatio(0.0f), nearPlane(0.1f), farPlane(1000.0f), view(1.0f), projection(1.0f), invProjection(1.0f), viewAndProj(1.0f)
+	Camera(float fov = 90.0f)
+		: fov(fov), aspectRatio(0.0f), nearPlane(0.1f), farPlane(1000.0f), view(1.0f), projection(1.0f), invProjection(1.0f), viewAndProj(1.0f), flipAspectScale(false)
 	{
 		oldTransform.position = glm::vec3(~0u);
 	}
 
 	void calculateProjectionMatrix(
-		float aspectRatio, 
-		float nearPlane = 0.1f, 
+		float aspectRatio,
+		float nearPlane = 0.1f,
 		float farPlane = 1000.0f)
 	{
-        this->aspectRatio = aspectRatio;
+		this->aspectRatio = aspectRatio;
 		this->nearPlane = nearPlane;
 		this->farPlane = farPlane;
 
-		this->projection = glm::perspective(glm::radians(fov), aspectRatio, nearPlane, farPlane);
+		this->getPerspectiveMatrix(glm::radians(fov), aspectRatio, nearPlane, farPlane, this->projection);
 		this->invProjection = glm::inverse(this->projection);
 		this->viewAndProj = this->projection * this->view;
 	}
@@ -52,5 +92,10 @@ public:
 			this->viewAndProj = this->projection * this->view;
 			oldTransform = transform;
 		}
+	}
+
+	void flipCameraAspectScale()
+	{
+		this->flipAspectScale = true;
 	}
 };
